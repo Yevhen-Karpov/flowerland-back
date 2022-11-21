e7bcd653dc1483b9a482598ccda877b34b711691
Yevhen-Karpov <goldblogart@gmail.com>
2022-11-21 22:25:06 +0200

    Update addAchimene.js

--- controllers/achimenes/addAchimene.js
+++ controllers/achimenes/addAchimene.js
@@ -2,8 +2,8 @@ const { Achimene } = require("../../models");
 
 const addAchimene = async (req, res, next) => {
   try {
-    const { _id } = req.user;
-    const achimene = await Achimene.create({ ...req.body, owner: _id });
+    // const { _id } = req.user;
+    const achimene = await Achimene.create(req.body);
     res.status(201).json({
       status: "success",
       code: 201,
