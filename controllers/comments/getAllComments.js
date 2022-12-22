1b0dee398b520ef3800c28b68e8b9cff6edbe268
Yevhen-Karpov <81827609+Yevhen-Karpov@users.noreply.github.com>
2022-12-22 14:41:04 +0200

    Update getAllComments.js

--- controllers/comments/getAllComments.js
+++ controllers/comments/getAllComments.js
@@ -2,7 +2,7 @@ const { Comment } = require("../../models");
 
 const getAllComments = async (req, res, next) => {
   try {
-    const comments = await Comment.find({}, {sort:-1,});
+    const comments = await Comment.find({});
     res.json({
       status: "success",
       code: 200,
