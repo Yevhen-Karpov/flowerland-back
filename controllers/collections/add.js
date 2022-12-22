a6516273719169e276253a6899f1dbe883836ce6
Yevhen-Karpov <81827609+Yevhen-Karpov@users.noreply.github.com>
2022-12-22 21:43:41 +0200

    Update add.js

--- controllers/collections/add.js
+++ controllers/collections/add.js
@@ -3,14 +3,14 @@ const { Collection } = require("../../models");
 const add = async (req, res, next) => {
   const { _id } = req.user;
   try {
-    const newCollection = await Collection.find({ name });
-    if (newCollection) {
-      return res.status(409).json({
-        status: "error",
-        code: 409,
-        message: "Collection already exists",
-      });
-    }
+//     const newCollection = await Collection.find({ name });
+//     if (newCollection) {
+//       return res.status(409).json({
+//         status: "error",
+//         code: 409,
+//         message: "Collection already exists",
+//       });
+//     }
 
     const result = await Collection.create({ ...req.body, owner: _id });
     res.status(201).json({
